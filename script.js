dcb4ceeff00b65f37d40e7ef05f33ec207d724ae
Dawid <szynanski@hotmail.com>
2023-08-08 16:57:14 +0200

    improvements to multiple functions

--- script.js
+++ script.js
@@ -1,29 +1,63 @@
 // Responsible for switching between dark and light mode.
 function darkLightMode() 
 {
-  //Get references to website sections.
+  //Get references to website's sections and objects.
   const body = document.body;
   const navBar = document.querySelector(".nav-bar");
   const footer = document.querySelector(".footer-section");
   const icons = document.getElementsByClassName("icon-change");
-  const sections = document.querySelectorAll("#section");
+  const sections = document.querySelectorAll(".section");
+  const sectionHeaders = document.querySelectorAll(".section-headers");
+  const cvLink = document.querySelectorAll(".resume-cv-link");
+  const repoLink = document.querySelectorAll(".project-repo-button");
+  const zoomText = document.querySelectorAll(".project-zoom-info");
 
-  //Using CSS visuals in general.css file.
-  body.classList.toggle("light-mode-body");
-  navBar.classList.toggle("light-mode-nav-bar");
-  footer.classList.toggle("light-mode-footer");
+  //Get reference to a colour change button.
+  const colourButton = document.querySelector(".color-mode-button");
 
-  //Loop needed as there are multiple sections with the same id.
-  for (let i = 0; i < sections.length; i++)
-  {
-    sections[i].classList.toggle("light-mode-sections");
-  }
-  
+  //Bolean that returns if Light Mode is on or off.
   const isLightMode = body.classList.contains("light-mode-body");
 
-  for (let i = 0; i < icons.length; i++) {
-    icons[i].className = isLightMode ? "icon-change fas fa-moon fa-beat" : "icon-change fas fa-sun fa-beat";
-  }
+  //Code below will trigger upon button click.
+  colourButton.addEventListener("click", function()
+  {
+    //Using CSS visuals in general.css file.
+    body.classList.toggle("light-mode-body");
+    navBar.classList.toggle("light-mode-nav-bar");
+    footer.classList.toggle("light-mode-footer");
+
+    //Loops are needed as there are multiple objects with the same classes.
+    for (let i = 0; i < sections.length; i++)
+    {
+      sections[i].classList.toggle("light-mode-sections");
+    }
+
+    for (let i = 0; i < sectionHeaders.length; i++)
+    {
+      sectionHeaders[i].classList.toggle("light-mode-header-borders");
+    }
+
+    for (let i = 0; i < cvLink.length; i++)
+    {
+      cvLink[i].classList.toggle("light-mode-cv-link");
+    }
+
+    for (let i = 0; i < repoLink.length; i++)
+    {
+      repoLink[i].classList.toggle("light-mode-repo-link");
+    }
+
+    for (let i = 0; i < zoomText.length; i++)
+    {
+      zoomText[i].classList.toggle("light-mode-zoom-text");
+    }
+
+    //Loop that switches icons depending on which mode is active.
+    for (let i = 0; i < icons.length; i++) 
+    {
+      icons[i].className = isLightMode ? "icon-change fas fa-moon fa-beat" : "icon-change fas fa-sun fa-beat";
+    }
+  });
 }
 
 // Responsible for page scrolling depending on which button has been clicked.
@@ -37,32 +71,69 @@ function pageScroll()
 
   //Set value to a variable used by "Top" button.
   const topPosition = 0;
-  //Get references to website sections.
-  const aboutPosition = document.querySelector(".welcome-section");
-  const resumePosition = document.querySelector(".about-section");
-  const projectsPosition = document.querySelector(".resume-section");
 
-  //Get a reference of navBar height to a variable that can be used to determine exact scroll position.
-  const navBarHeight = document.querySelector(".nav-bar").offsetHeight;
+  //Function that returns fresh NavBar height calculation.
+  const CalculateNavBarHeight = () => 
+  {
+    return document.querySelector(".nav-bar").offsetHeight;
+  }
 
-  //Usage of a function "scrollToPosition" to avoid code duplication.
-  scrollToPosition(topButton, topPosition);
-  scrollToPosition(aboutButton, aboutPosition.offsetTop + aboutPosition.offsetHeight - navBarHeight);
-  scrollToPosition(resumeButton, resumePosition.offsetTop + resumePosition.offsetHeight - navBarHeight);
-  scrollToPosition(projectsButton, projectsPosition.offsetTop + projectsPosition.offsetHeight - navBarHeight);
-}
-
-// Supporting function to pageScroll that reduces code duplication.
-function scrollToPosition(button, position) 
-{
-  //Button click will trigger a smooth scroll to passed position.
-  button.addEventListener("click", function() 
+  //Code below will trigger upon button click.
+  topButton.addEventListener("click", function() 
   {
     window.scrollTo(
     {
-      top: position,
+      top: topPosition,
       behavior: "smooth"
     });
+  });
+
+  //Code below will trigger upon button click.
+  aboutButton.addEventListener("click", function() 
+  {
+    //Get references to website sections.
+    const aboutPosition = document.querySelector(".welcome-section");
+
+    //Get a reference of navBar height to a variable.
+    const navBarHeight = CalculateNavBarHeight();
+
+    window.scrollTo(
+    {
+      top: aboutPosition.offsetTop + aboutPosition.offsetHeight - navBarHeight,
+      behavior: "smooth"
+    });
+  });
+
+  //Code below will trigger upon button click.
+  resumeButton.addEventListener("click", function() 
+  {
+    //Get references to website sections.
+    const resumePosition = document.querySelector(".about-section");
+
+    //Get a reference of navBar height to a variable.
+    const navBarHeight = CalculateNavBarHeight();
+
+    window.scrollTo(
+      {
+        top: resumePosition.offsetTop + resumePosition.offsetHeight - navBarHeight,
+        behavior: "smooth"
+      });
+  });
+
+  //Code below will trigger upon button click.
+  projectsButton.addEventListener("click", function() 
+  {
+    //Get references to website sections.
+    const projectsPosition = document.querySelector(".resume-section");
+
+    //Get a reference of navBar height to a variable.
+    const navBarHeight = CalculateNavBarHeight();
+
+    window.scrollTo(
+      {
+        top: projectsPosition.offsetTop + projectsPosition.offsetHeight - navBarHeight,
+        behavior: "smooth"
+      });
   });
 }
 
@@ -128,48 +199,40 @@ function toggleLanguage(language)
   const polishBtn = document.getElementsByClassName("polish-language-button")[0];
 
   //Usage of function "languageBtnAnimation" for grayscale animation (both ways).
-  languageBtnAnimation (englishBtn, polishBtn);
-  languageBtnAnimation (polishBtn, englishBtn);
+  languageBtnAnimation(englishBtn, polishBtn);
+  languageBtnAnimation(polishBtn, englishBtn);
 
   //Stores references to all elements with a special class added to all text that require translations.
   const elements = document.getElementsByClassName('lang-toggle');
 
   //Fetch translations from JSON file.
-  fetch('languages.json')
-    .then(response => response.json())
-    .then(data => 
+  fetch('languages.json').then(response => response.json()).then(data => 
+  {
+    //Loop through all text that contains "lang-toggle" class.
+    for (let i = 0; i < elements.length; i++) 
     {
-      //Loop through all text that contains "lang-toggle" class.
-      for (let i = 0; i < elements.length; i++) 
+      //Represents each individual element with "lang-toggle" class.
+      const element = elements[i];
+      //Each translated text received a "data-key" that has assigned English and Polish text in JSON file.
+      const key = element.getAttribute('data-key');
+      //Stores default language values.s
+      const defaultText = data.english[key];
+      //Stores translated (Polish) language values.
+      const translatedText = data[language][key];
+
+      if (translatedText) 
       {
-        //Represents each individual element with "lang-toggle" class.
-        const element = elements[i];
-        //Each translated text received a "data-key" that has assigned English and Polish text in JSON file.
-        const key = element.getAttribute('data-key');
-        //Stores default language values.
-        const defaultText = data.english[key];
-        //Stores translated (Polish) language values.
-        const translatedText = data[language][key];
+        //Update language if translatedText exists.
+        element.innerHTML = translatedText;
+      } 
 
-        if (translatedText) 
-        {
-          //Update language if translatedText exists.
-          element.innerHTML = translatedText;
-        } 
-
-        else 
-        {
-          //Use default English text if not.
-          element.textContent = defaultText;
-        }
+      else 
+      {
+        //Use default English text if not.
+        element.textContent = defaultText;
       }
-    })
-
-    //Safety catch block that will provide an error if there is a translation problem.
-    .catch(error => 
-    {
-      console.error('Error loading language data:', error);
-    });
+    }
+  })
 }
 
 // Supporting function to toggleLanguage that changes button animations.
@@ -250,10 +313,20 @@ function fadeOutAndIn (fadeOut, fadeIn)
 // Used to load below functions after page refresh so that first button click triggers them properly.
 document.addEventListener('DOMContentLoaded', function() 
 {
+  darkLightMode();
   pageScroll();
   websiteLinks();
   repositoryLinks();
-  toggleLanguage();
+  toggleLanguage("english");
   resumeDisplay();
   projectsDisplay();
+
+  //Added button click events to Language Buttons.
+  document.querySelector(".english-language-button").addEventListener("click", function() {
+    toggleLanguage('english');
+  });
+  
+  document.querySelector(".polish-language-button").addEventListener("click", function() {
+    toggleLanguage('polish');
+  });
 });
